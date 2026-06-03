a11a183df73dac97845d444eca18e3e0d5340ba0
souzzdev <ninjasdttn@gmail.com>
2026-06-03 15:17:57 -0300

    feat: implement user creation method

--- src/main/java/com/eranitor/tcc/services/UsuarioService.java
+++ src/main/java/com/eranitor/tcc/services/UsuarioService.java
@@ -1,7 +1,32 @@
 package com.eranitor.tcc.services;
 
+import com.eranitor.tcc.dto.ErrorResponseDTO;
+import com.eranitor.tcc.entity.Usuario;
+import com.eranitor.tcc.repository.UsuarioRepositoy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
+    @Autowired
+    UsuarioRepositoy repository;
+
+    public ResponseEntity<?> cadastrarUsuario (Usuario usuario) {
+        if (repository.existsById(usuario.getIdUsuario())) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(new ErrorResponseDTO(
+                            409,
+                            "O usuário já existe!"
+                    ));
+        }
+
+        Usuario usuarioSalvo = repository.save(usuario);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(usuarioSalvo);
+    }
 }
